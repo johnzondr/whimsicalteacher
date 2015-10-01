c32228adaa75804231b0af930350737833bfcd80
John Zhao <john@zondr.com>
2015-10-01 09:23:19 +0000

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Whimsicalteacher::Application.config.secret_key_base = '09f7f3e2111c736be567f23a9266a464a8035d14b0a337262c9295a569f432ad8919903ccf49ca1af693255033fae1def0c356d93b37afbfe2a87e265173903a'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Whimsicalteacher::Application.config.session_store :cookie_store, key: '_whimsicalteacher_session'
